18cbfd8caabd0c99ed173c0c0908d7bfb923024e
Allison Grekin <allison_grekin@hotmail.com>
2017-02-13 16:52:20 +0000

    completed ex 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'game', to: 'word#game'
+
+  get 'score', to: 'word#score'
+
+  root to: 'word#game', as: :welcome
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
